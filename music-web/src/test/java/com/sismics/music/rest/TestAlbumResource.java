e4cd1a0164c39f08277f0e7e8efc3641d936080e
jendib <benjamin.gam@gmail.com>
2016-05-05 23:17:16 +0200

    Fix JUnit

--- music-web/src/test/java/com/sismics/music/rest/TestAlbumResource.java
+++ music-web/src/test/java/com/sismics/music/rest/TestAlbumResource.java
@@ -100,7 +100,7 @@ public class TestAlbumResource extends BaseJerseyTest {
         json = target().path("/album/" + album0Id + "/albumart").request()
                 .cookie(TokenBasedSecurityFilter.COOKIE_NAME, adminAuthenticationToken)
                 .post(Entity.form(new Form()
-                        .param("url", "http://www.sismics.com/img/sismics.png")), JsonObject.class);
+                            .param("url", "http://lorempixel.com/200/200/")), JsonObject.class);
         Assert.assertNull(json.get("message"));
         
         // Get an album art
@@ -120,7 +120,7 @@ public class TestAlbumResource extends BaseJerseyTest {
         json = target().path("/album/" + album0Id + "/albumart").request()
                 .cookie(TokenBasedSecurityFilter.COOKIE_NAME, adminAuthenticationToken)
                 .post(Entity.form(new Form()
-                        .param("url", "http://www.sismics.com/img/sismics.png")), JsonObject.class);
+                        .param("url", "http://lorempixel.com/200/200/")), JsonObject.class);
         Assert.assertEquals("AlbumArtNotCopied", json.getString("message"));
         albumArtFile.setWritable(true);
         
