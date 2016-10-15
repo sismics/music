fa628f4a4ba620d312f87194e2f15e9692f50c57
jendib <benjamin.gam@gmail.com>
2016-10-15 18:01:11 +0200

    Remove Sismics specific scripts

--- run-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker rm -f sismics_music
-docker run \
-    -d --name=sismics_music --restart=always \
-    -v sismics_music_data:/data \
-    -e 'VIRTUAL_HOST=music.sismics.com' -e 'VIRTUAL_PORT=80' \
-    sismics/music:latest
